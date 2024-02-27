0db0e3b5fbbcbf681102a9146e89e761968f0432
6badkarma6 <il2grinkon@gmail.com>
2024-02-27 20:27:53 +0300

    bot_v0.2b

---
+++ exel.py
@@ -0,0 +1,177 @@
+
+def rw(gr: str):
+    data = ajson()
+    return message(nn(lists=message_dt(gr=gr, load_file=data['sso'])))
+
+
+def nn(lists: list) -> list:
+    l = 0
+    for j in lists:
+        l += 1
+        n = 0
+        for k in j:
+            if k == 'None' and j.index(k) != 4:
+                print(j)
+                lists[lists.index(j)][j.index(k)] = lists[lists.index(j)-1][j.index(k)]
+    return lists
+
+
+def message(data: list) -> str:
+    ms = ''
+    for i in data:
+        for w in i:
+            ms = ms + w
+    return ms
+
+
+def message_dt(gr: str, load_file: str, file: str = 'data.json') -> list:
+    import openpyxl
+    wb = openpyxl.load_workbook(load_file)
+    sh = wb.active
+    data = ajson(file)
+    message_data = []
+    xd = data[gr]['x']
+    yd = data[gr]['y']
+    for y in yd:
+        dt = []
+        for x in xd:
+            if str(sh[f'{x}{y}'].value) == 'None' and x != data[gr]['x'][0] and y != data[gr]['y'][0]: #  and x != data[gr]['x'][3] and y != data[gr]['y'][3]:
+                dt.append(str(sh[f'{x}{y}'].value))
+                # dt.append('     ')
+            elif str(sh[f'{x}{y}'].value) == 'None' and x != data[gr]['x'][0] and y != data[gr]['y'][0]: # and x != data[gr]['x'][4] and y != data[gr]['y'][3]:
+                dt.append(str(sh[f'{x}{y}'].value))
+                # dt.append('     ')
+            elif str(sh[f'{x}{y}'].value) == 'None':
+                dt.append('     ')
+            else:
+                dt.append(str(sh[f'{x}{y}'].value) + '    ')
+        dt.append('\n')
+        message_data.append(dt)
+    return message_data
+
+
+def src():
+    import os
+    os.system("curl -o page_source.html https://vgke.by/raspisanie-zanjatij/")
+
+
+def pr():
+    from bs4 import BeautifulSoup
+    import json
+    with open("page_source.html") as html:
+        print('pr start')
+        soup = BeautifulSoup(html, 'html.parser')
+        data = []
+        http = []
+        name = []
+        for link in soup.find_all('iframe'):
+             data.append(link.get('src'))
+        for i in range(len(data)):
+            ds = i
+            http.append(data[ds][51:])
+            if data[ds][-17] == '/':
+                name.append(data[ds][-16:])
+            elif data[ds][-13] == '/':
+                name.append(data[ds][-12:])
+            elif data[ds][-15] == '/':
+                name.append(data[ds][-14:])
+            else:
+                name.append(data[ds][-15:])
+            if name[ds][:7] == 'den-sso':
+                sso = str(data[ds][-12:])
+                http_sso = str(data[ds][51:])
+            if name[ds][:7] == 'den-sso' and name[ds][7] == '-':
+                sso = str(data[ds][-14:])
+                http_sso = str(data[ds][51:])
+        print('pr finish')
+        with open('conf.json', 'w') as write_file:
+            json.dump({'http': http, 'http_sso': http_sso, 'name': name, 'sso': sso}, write_file)
+        return True
+
+
+def save(pb=False):
+    import os
+    if pb:
+        pr()
+        print('pr True')
+    print('start...')
+    data = ajson()
+    # os.system('cd /mnt/d/Tg/download')
+    os.system("curl -O " + data['http_sso'])
+    print('...finish')
+
+
+def ajson(file: str = 'conf.json'):
+    import json
+    with open(file, 'r') as file:
+        date = json.load(file)
+    return date
+
+
+def sort(file: str):
+	import openpyxl
+	file = openpyxl.load_workbook(file)
+	open_file = file.active
+	sn = {}
+	i = 2
+	while True:
+		r = 5
+		r += i
+		if open_file.cell(row=r, column=3).value ==' ':
+			i += 1
+			sn['y'] = i
+			break
+		i += 2
+	o = 1
+	while True:
+		l = 3
+		l += o
+		if open_file.cell(row=4, column=l).value != 'None':
+			o += 1
+			sn['x'] = o
+			break
+		o += 1
+	for i in range(1,open_file.max_row):
+		for j in range(1,open_file.max_column):
+			if open_file.cell(row=i, column=j).value == 'ПО-1':
+				sn['po-r'] = [4, i]
+				sn['po-a'] = [3, j]
+			if open_file.cell(row=i, column=j).value == 'ЭС-2':
+				sn['es-r'] = [4, i]
+				sn['es-a'] = [3, j]
+	return sn
+
+
+def ls(a: int, r: int, x: int = 3, y: int = 7) -> list:
+	abc = ['', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+	row = []
+	column = []
+	for i in range(0,x):
+		k = abc[a]
+		column.append(f'{k}')
+		a += 1
+	for j in range(0,y):
+		row.append(f'{r}')
+		r += 1
+	return column, row
+
+
+def data_conf():
+	with open('data.json', 'w') as file:
+		import json
+		data = ajson()
+		sn = sort(data['sso'])
+		po_x, trash = ls(sn['po-a'][0], sn['po-r'][0], y=sn['y'], x=sn['x'])
+		po_z, po_y = ls(sn['po-a'][1], sn['po-r'][1], y=sn['y'])
+		po_x = po_x + po_z
+		es_x, trash = ls(sn['es-a'][0], sn['es-r'][0], y=sn['y'], x=sn['x'])
+		es_z, es_y = ls(sn['es-a'][1], sn['es-r'][1], y=sn['y'])
+		es_x = es_x + es_z
+		json.dump({'ПО-1': {'x': po_x, 'y': po_y},
+			   'ЭС-2': {'x': es_x, 'y': es_y}}, file)
+
+# if __name__ == "__main__":
+#    pr()
+#    save()
+
+
---
+++ tg_bot.py
@@ -0,0 +1,48 @@
+import telebot
+from telebot import types
+
+from exel import rw
+
+bot = telebot.TeleBot('6788562469:AAFvF-l8F9cJrUJeYSyCsLdnPR7TbyyqbS8')
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    print('User ' + str(message.from_user.id))
+    c1 = types.BotCommand(command='start', description='Start the Bot')
+    c2 = types.BotCommand(command='gr1', description='Расписание ПО-1')
+    c3 = types.BotCommand(command='gr2', description='Расписание ЭС-2')
+    bot.set_my_commands([c1, c2, c3])
+    markup = types.InlineKeyboardMarkup()
+    #
+    #markup.add(types.InlineKeyboardButton("Расписание ПО-1", callback_data='study-po-1'))
+    #
+    #markup.add(types.InlineKeyboardButton("Расписание ЭС-2", callback_data='study-es-2'))
+    #
+    bot.send_message(message.from_user.id, ' Bot v0.2b \n Работает с 15:00 по 21:00', reply_markup=markup)
+
+
+'''@bot.callback_query_handler(func=lambda callback: True)
+def study(callback):
+    if callback.data == 'study-es-2':
+	# with open('user.txt', 'a') as file:
+	# 	file.write(callback.message.from_user.id)
+	# print('User: ' + str(callback.message.from_user.id))
+        bot.send_message(callback.message.chat.id, gr2(callback))
+    if callback.data == 'study-po-1':
+        bot.send_message(callback.message.chat.id, gr1(callback))'''
+
+
+@bot.message_handler(commands=['gr1'])
+def gr1(message):
+    print('User ' + str(message.from_user.id))
+    bot.send_message(message.from_user.id, rw(gr='ПО-1'))
+
+
+@bot.message_handler(commands=['gr2'])
+def gr2(message):
+    print('User ' + str(message.from_user.id))
+    bot.send_message(message.from_user.id, rw(gr='ЭС-2'))
+
+
+
+bot.polling(none_stop=True)
