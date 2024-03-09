998effd0fc2662d2e8cb8e20a2616e2b73c2db51
6badkarma6 <il2grinkon@gmail.com>
2024-03-09 19:45:05 +0300

    exel v 0.3b

--- tg_bot.py
+++ tg_bot.py
@@ -1,13 +1,20 @@
-import telebot
-from telebot import types
-from exel import rw, data
+# Проверка на наличие библиотек
+try:
+    import telebot
+    from telebot import types
+    from exel import rw, data
+except Exception as error:
+    raise error
+finally:
+    print("Функции успешно импортированны")
 
-
+# Взятие токена и его инициализация
 bot = telebot.TeleBot(data())
 
 
 @bot.message_handler(commands=['start'])
 def start(message):
+    """Функция настройки бота при его старте"""
     print('User ' + str(message.from_user.id))
     c1 = types.BotCommand(command='start', description='Start the Bot')
     c2 = types.BotCommand(command='gr1', description='Расписание ПО-1')
@@ -19,39 +26,57 @@ def start(message):
     bot.send_message(message.from_user.id, ' Bot v0.3b \n Работает с 15:00 по 21:00', reply_markup=markup)
 
 
+# Отвечает на callback.data
 @bot.callback_query_handler(func=lambda callback: True)
 def study(callback):
+    """Отвечает на InLineKeyboardButton"""
     if callback.data == 'study-po-1':
         print('User in callback po-1: ' + str(callback.from_user.id))
-        bot.send_message(callback.from_user.id, rw(gr='ПО-1'), protect_content=True, disable_notification=True)
+        bot.send_message(callback.from_user.id, rw(gr='ПО-1'),
+                         protect_content=True,
+                         disable_notification=True)
     if callback.data == 'study-es-2':
         print('User in callback es-2: ' + str(callback.from_user.id))
-        bot.send_message(callback.from_user.id, rw(gr='ЭС-2'), protect_content=True, disable_notification=True)
+        bot.send_message(callback.from_user.id, rw(gr='ЭС-2'),
+                         protect_content=True,
+                         disable_notification=True)
 
 
+# Выводит рассписание группы номер 1
 @bot.message_handler(commands=['gr1'])
 def gr1(message):
+    """Рассписание группы номер 1"""
     print('User in gr1: ' + str(message.from_user.id))
-    bot.send_message(message.from_user.id, rw(gr='ПО-1'), protect_content=True, disable_notification=True)
+    bot.send_message(message.from_user.id, rw(gr='ПО-1'),
+                     protect_content=True,
+                     disable_notification=True)
 
 
+# Выводит рассписание группы номер 2
 @bot.message_handler(commands=['gr2'])
 def gr2(message):
+    """Рассписание группы номер 2"""
     print('User in gr2: ' + str(message.from_user.id))
-    bot.send_message(message.from_user.id, rw(gr='ЭС-2'), protect_content=True, disable_notification=True)
+    bot.send_message(message.from_user.id, rw(gr='ЭС-2'),
+                     protect_content=True,
+                     disable_notification=True)
 
 
 @bot.message_handler(commands=['adm'])
 def adm(message):
+    """Админ панель для тестов"""
     if message.chat.id == 1474943294:
         markup = types.InlineKeyboardMarkup()
         markup.add(types.InlineKeyboardButton("Расписание ПО-1", callback_data='study-po-1'))
-        bot.send_message(message.from_user.id, message, reply_markup=markup,
-                         protect_content=True, disable_notification=True)
+        bot.send_message(message.from_user.id, message,
+                         reply_markup=markup,
+                         protect_content=True,
+                         disable_notification=True)
 
 
 print('bot start')
 try:
-    bot.polling(none_stop=True, timeout=10000)
+    bot.polling(none_stop=True,
+                timeout=10000)
 finally:
     print('bot finish')
