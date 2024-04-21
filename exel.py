085ddfe15c288bc733170ce25de7f233d48ae63a
6badkarma6 <il2grinkon@gmail.com>
2024-04-21 22:21:21 +0300

    bot_v1

--- exel.py
+++ exel.py
@@ -1,42 +1,71 @@
+"""Модули для бота"""
 
-def rw():
+
+def lon(data) -> str:
+    string = ' Список групп: '
+    for i in range(len(data)):
+        string = string + data[i] + ' '
+    return string
+
+
+def rw(group: str) -> str:
+    """Вывод строки с рассписанием группы"""
+    # data = ajson()
+    # return message(nn(lists=message_dt(gr=gr, load_file=data['sso'])))
+    groups = ajson("timetable.json")
+    return groups[group]
+
+
+def time_grs():
+    import json
+    time = {}
     data = ajson()
-    return message(nn(lists=message_dt(load_file=data['sso'])))
+    list_grs = ajson('bin.json')
+    list_grs = list_grs['grs']
+    for i in list_grs:
+        print(type(i), i)
+        time[i] = message(nn(lists=message_dt(gr=i, load_file=data['sso'])))
+    print(time)
+    with open('timetable.json', 'w') as file:
+        json.dump(time, file, indent=2)
+    return True
 
 
+# Фильтр
 def nn(lists: list) -> list:
-    for j in lists:
-        for k in j:
-            if k == 'None':
-                lists[lists.index(j)][j.index(k)] = lists[lists.index(j)-1][j.index(k)]
+    """Фильтрует от пустых строк и добавляет недостоющие"""
+    for j in range(len(lists)):
+        for k in range(len(lists[j])):
+            if lists[j][k] == 'None' and j != 3:
+                lists[j][k] = lists[j - 1][k]
+    for j in range(len(lists)):
+        for k in range(len(lists[j])):
+            if lists[j][k] == 'None':
+                lists[j][k] = '     '
     return lists
 
 
+# Форматирует список в текст
 def message(data: list) -> str:
-    ms = ''
-    for i in data:
-        for w in i:
-            ms = ms + w
-    return ms
+    """Преобразует список в текст"""
+    return ''.join([''.join(dt) for dt in data])
 
 
-def message_dt(load_file: str, file: str = 'data.json') -> list:
+# Формирует список
+def message_dt(gr: str, load_file: str, file: str = 'data.json') -> list:
+    """Формирует список из .xlsx файла по конфигурации data.json"""
     import openpyxl
     wb = openpyxl.load_workbook(load_file)
     sh = wb.active
     data = ajson(file)
     message_data = []
-    xd = data['x']
-    yd = data['y']
+    xd = data[gr]['x']
+    yd = data[gr]['y']
     for y in yd:
         dt = []
         for x in xd:
-            if str(sh[f'{x}{y}'].value) == 'None' and x != data['x'][0] and y != data['y'][0] and x != data['x'][3]:
-                # dt.append(str(sh[f'{x}{y}'].value))
-                dt.append('     ')
-            elif str(sh[f'{x}{y}'].value) == 'None' and x != data['x'][0] and y != data['y'][0] and x != data['x'][4]:
-                # dt.append(str(sh[f'{x}{y}'].value))
-                dt.append('     ')
+            if str(sh[f'{x}{y}'].value) == 'None' and x != data[gr]['x'][0] and y != data[gr]['y'][0]:
+                dt.append(str(sh[f'{x}{y}'].value))
             elif str(sh[f'{x}{y}'].value) == 'None':
                 dt.append('     ')
             else:
@@ -46,18 +75,20 @@ def message_dt(load_file: str, file: str = 'data.json') -> list:
     return message_data
 
 
+# Сохраняет HTML формат страницы
 def src():
+    """Сохраняет исходный код страницы"""
     import os
-    os.system("curl -o page_source.html " + "https://vgke.by/raspisanie-zanjatij/")
+    os.system("curl -o page_source.html https://vgke.by/raspisanie-zanjatij/")
+    return True
 
 
+# Достаёт из исходного кода нужные ссылки и названия
 def pr():
+    """Достаёт из исходного кода нужные ссылки и названия"""
     from bs4 import BeautifulSoup
     import json
-    print('src start')
-    src()
-    print('src finish')
-    with open("page_source.html") as html:
+    with open(file='page_source.html', mode='r', encoding='utf-8') as html:
         print('pr start')
         soup = BeautifulSoup(html, 'html.parser')
         data = []
@@ -65,9 +96,8 @@ def pr():
         name = []
         for link in soup.find_all('iframe'):
             data.append(link.get('src'))
-
-        for i in range(len(data)):
-            ds = i
+        for j in range(len(data)):
+            ds = j
             http.append(data[ds][51:])
             if data[ds][-17] == '/':
                 name.append(data[ds][-16:])
@@ -78,31 +108,199 @@ def pr():
             else:
                 name.append(data[ds][-15:])
             if name[ds][:7] == 'den-sso':
-                sso = data[ds][-12:]
-                http_sso = data[ds][51:]
+                sso = str(data[ds][-12:])
+                http_sso = str(data[ds][51:])
             if name[ds][:7] == 'den-sso' and name[ds][7] == '-':
-                sso = data[ds][-14:]
-                http_sso = data[ds][51:]
+                sso = str(data[ds][-14:])
+                http_sso = str(data[ds][51:])
         print('pr finish')
         with open('conf.json', 'w') as write_file:
-            json.dump({'http': http, 'http-sso': http_sso, 'name': name, 'sso': sso}, write_file)
+            json.dump({'http': http,
+                       'http_sso': http_sso,
+                       'name': name,
+                       'sso': sso}, write_file, indent=2)
         return True
 
 
+# Сохраняет файл
 def save(pb=False):
+    """Сохраняет файл"""
     import os
     if pb:
         pr()
         print('pr True')
     print('start...')
     data = ajson()
-    # os.system('cd /mnt/d/Tg/download')
-    os.system("curl -O " + data['http-sso'])
+    os.system("curl -O " + data['http_sso'])
     print('...finish')
 
 
+# Импортирует данные файла conf.json
 def ajson(file: str = 'conf.json'):
+    """Импортирует данные"""
     import json
     with open(file, 'r') as file:
-        date = json.load(file)
-    return date
+        data = json.load(file)
+    return data
+
+
+# Сортирует exel файл
+def search(file: str, gr: str):
+    """Сортирует exel файл"""
+    import openpyxl
+    workbook = openpyxl.load_workbook(file)
+    open_file = workbook.active
+    for gr_x in range(1, open_file.max_row):
+        for gr_y in range(1, open_file.max_column):
+            if open_file.cell(row=gr_x, column=gr_y).value == gr:
+                return gr_x, gr_y
+
+
+def filters(file: str, gr_x: int, gr_y: int, gr: str):
+    # data = ajson('bin.json')
+    import openpyxl
+    workbook = openpyxl.load_workbook(file)
+    open_file = workbook.active
+    x = gr_x
+    # print(x, end=' ')
+    y = 3
+    # print(y)
+    for i in range(14):
+        x += 1
+        if str(open_file.cell(row=x, column=y).value) == '' or str(open_file.cell(row=x, column=y).value) == ' ':
+            # print('break', x, open_file.cell(row=x, column=y).value)
+            x -= 1
+            if gr == 'ЭС-2':
+                x -= 1
+            break
+        # print(x, open_file.cell(row=x, column=y).value)
+    for i in range(4):
+        if str(open_file.cell(row=gr_x, column=y).value) == 'None':
+            # print('break')
+            break
+        y += 1
+        # print(y, open_file.cell(row=x, column=gr_y).value)
+    if str(open_file.cell(row=gr_x, column=g(1, gr_y)).value) == 'None' \
+            and str(open_file.cell(row=gr_x, column=g(3, gr_y)).value) == 'None':
+        gr_y += 3
+    elif str(open_file.cell(row=gr_x, column=g(1, gr_y)).value) == 'None' \
+            and str(open_file.cell(row=gr_x, column=g(3, gr_y)).value) != 'None' or \
+            str(open_file.cell(row=gr_x, column=g(1, gr_y)).value) != 'None' \
+            and str(open_file.cell(row=gr_x, column=g(2, gr_y)).value) == 'None':
+        gr_y += 2
+    else:
+        gr_y += 1
+    return x, y, gr_y
+
+
+def g(x: int, y: int):
+    return x + y
+
+
+# Состовляет столбцы и строки
+def ls(gr_x: int, gr_y1: int, gr_y2, x: int, y: int, y0: int = 3, k: int = 1) -> tuple[list[str], list[str]]:
+    # [gr_x, 3, x, y], [gr_x, gr_y1, x, gr_y2]
+    import json
+    with open('bin.json', 'r') as file:
+        data = json.load(file)
+    abc = data['abc']
+    row = []
+    column = []
+    r_glob = x - gr_x + k
+    c_timing = y - y0 + k
+    c_gr = gr_y2 - gr_y1 + k
+    for n in range(0, r_glob):
+        row.append(str(g(gr_x, n)))
+    for i in range(0, c_timing):
+        column.append(abc[g(y0, i)])
+    for j in range(0, c_gr):
+        column.append(abc[g(gr_y1, j)])
+    return column, row
+
+
+# Формирует конфигурационный файл data.json
+def data_conf():
+    import json
+    conf = ajson()
+    binarn = ajson('bin.json')
+    grs = binarn['grs']
+    file = conf['sso']
+    with open(file='data.json', mode='w', encoding='utf-8') as dump_file:
+        dump = {}
+        for gr in grs:
+            print("start ", gr)
+            gr_x, gr_y1 = search(file, gr)
+            x, y, gr_y2 = filters(file, gr_x, gr_y1, gr)
+            column, row = ls(gr_x=gr_x, gr_y1=gr_y1, x=x, y=y, gr_y2=gr_y2)
+            dt = dict(x=column, y=row)
+            print(dt, end='\n\n')
+            dump[gr] = dt
+        print(dump)
+        json.dump(obj=dump, fp=dump_file, indent=4)
+    return time_grs()
+
+
+# Считывет токин бота
+def data() -> str:
+    with open('bot', 'r') as data_bot:
+        return data_bot.read(46)
+
+
+def info(message, name):
+    import datetime
+    print(datetime.datetime.fromtimestamp(message.date), end=': ')
+    print(name.from_user.first_name, end=' ')
+    print(name.from_user.id)
+
+
+def bol_user(user_id: int) -> bool:
+    with open('user_id.txt', mode='r', encoding='utf-8') as file:
+        ids = str(file.read())
+        print(ids)
+        ids = ids.split('\n')
+        print(ids)
+    if str(user_id) in ids:
+        return True
+    print(type(user_id), user_id)
+    return False
+
+
+class Users:
+    def __init__(self):
+        self.ids = None
+
+    def read_user_id(self):
+        with open("", mode='r') as file:
+            lists = file.read()
+            strings: list[str] = lists.split(" ")
+            for strs in strings:
+                if strs == "":
+                    continue
+                strs = int(strs)
+                self.ids.append(strs)
+
+    def check_id(self, user_id: int) -> bool:
+        if user_id in self.ids:
+            return True
+        else:
+            return False
+
+    @staticmethod
+    def add_user_id(user_id: int):
+        with open("", mode='a') as file:
+            file.write(f'{user_id} ')
+
+# if __name__ == "__main__":
+# import json
+# file = "den-sso-1.xlsx"
+# gr = str(input('>>'))
+# gr_x, gr_y1 = search(file, gr)
+# x, y, gr_y2 = filter(file, gr_x, gr_y1, gr)
+# print([gr_x, 3, x, y], [gr_x, gr_y1, x, gr_y2])
+# column, row = ls(gr_x=gr_x, gr_y1=gr_y1, x=x, y=y, gr_y2=gr_y2)
+# print(column, row)
+# data_conf()
+# print(rw('ПО-1'))
+# print(user(1234))
+# inp = int(input('>>'))
+# print(user(inp))
--- tg_bot.py
+++ tg_bot.py
@@ -1,27 +1,130 @@
-import telebot
-from telebot import types
+"""БОТ в Телеграмм"""
 
-from exel import rw
+# Проверка на наличие библиотек
+try:
+    import telebot
+    from telebot import types
+    from exel import rw, data, info, ajson, lon, Users
+except Exception as error:
+    raise error
+finally:
+    print("Функции успешно импортированны")
 
-bot = telebot.TeleBot('6788562469:AAFvF-l8F9cJrUJeYSyCsLdnPR7TbyyqbS8')
+# Взятие токена и его инициализация
+bot = telebot.TeleBot(data())
 
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    markup = types.InlineKeyboardMarkup()
-    markup.add(types.InlineKeyboardButton("Расписание", callback_data='study'))
-    bot.send_message(message.from_user.id, ' Bot v0.1a', reply_markup=markup)
+    """Функция настройки бота при его старте"""
+    print('User ', end=' ')
+    info(message, message)
+    c1 = types.BotCommand(command='start', description='Start the Bot')
+    c2 = types.BotCommand(command='gr', description='Print time for group')
+    c3 = types.BotCommand(command='list', description='Return list of group')
+    c4 = types.BotCommand(command='help', description='Return help doc')
+    bot.set_my_commands([c1, c2, c3, c4])
+    try:
+        bot.send_message(message.from_user.id, ' Bot v1 \n  работает',
+                         protect_content=True,
+                         disable_notification=True)
+    except BaseException:
+        print('exept')
 
 
+# Отвечает на callback.data
 @bot.callback_query_handler(func=lambda callback: True)
 def study(callback):
-    if callback.data == 'study':
-        bot.send_message(callback.message.chat.id, rw(), parse_mode='html')
+    """Отвечает на InLineKeyboardButton"""
+    if callback.data == 'study-po-1':
+        print('User in callback po-1: ', end='')
+        info(callback.message, callback)
+        try:
+            bot.send_message(callback.from_user.id, rw(group='ПО-1'),
+                             protect_content=True,
+                             disable_notification=True)
+        except BaseException:
+            print('exept')
+    if callback.data == 'study-es-2':
+        print('User in callback es-2: ', end='')
+        info(callback.message, callback)
+        try:
+            bot.send_message(callback.from_user.id, rw(group='ЭС-2'),
+                             protect_content=True,
+                             disable_notification=True)
+        except BaseException:
+            print('exept')
 
 
-@bot.message_handler(commands=['stop'])
-def stop():
-    raise SystemError('Program stopped')
+# Выводит рассписание группы номер 1
+@bot.message_handler(commands=['list'])
+def list(message):
+    """Рассписание группы номер 1"""
+    print('User in list: ', end='')
+    info(message, message)
+    data = ajson('bin.json')
+    try:
+        bot.send_message(message.from_user.id, lon(data['grs']),
+                         protect_content=True,
+                         disable_notification=True)
+    except BaseException:
+        print('exept')
 
 
-bot.polling(none_stop=True)
+
+@bot.message_handler(commands=['help'])
+def help(message):
+    """Рассписание группы номер 2"""
+    print('User in help: ', end='')
+    info(message, message)
+    doc = """
+    * для начало работы и сброса пред настрое бота ввежите '/start'
+    * для вывода списка доступных групп введите '/list'
+    * для вывода раписания для доступных группы введите '/gr -*-'
+    -*- — группа из списка групп
+"""
+    try:
+         bot.send_message(message.from_user.id, doc,
+                          protect_content=True,
+                          disable_notification=True)
+    except BaseException:
+         print('exept')
+
+
+@bot.message_handler(commands=['gr'])
+def gr(message):
+    """Рассписание групп"""
+    print('User in gr: ', end='')
+    info(message, message)
+    text = message.text.split(' ')
+    if len(text) == 1:
+       bot.send_message(message.from_user.id, 'Не правильно введена команда',
+                         protect_content=True,
+                         disable_notification=True)
+    try:
+        group = text[1].upper()
+        print(group)
+        bot.send_message(message.from_user.id, rw(group=group),
+                         protect_content=True,
+                         disable_notification=True)
+    except BaseException:
+        print('exept')
+
+
+@bot.message_handler(commands=['adm'])
+def adm(message):
+    """Админ панель для тестов"""
+    if message.chat.id == 1474943294:
+        markup = types.InlineKeyboardMarkup()
+        markup.add(types.InlineKeyboardButton("Расписание ПО-1", callback_data='study-po-1'))
+        bot.send_message(message.from_user.id, message,
+                         reply_markup=markup,
+                         protect_content=True,
+                         disable_notification=True)
+
+
+print('bot start')
+try:
+    bot.polling(none_stop=True, timeout=10000)
+finally:
+    print('bot finish')
